c54f9cef4f9b7d299a867e3aaa517be5ee4cc4b8
Aizendaisho <aizendaisho@gmail.com>
2022-10-05 19:17:31 -0400

    faltan

--- index.js
+++ index.js
@@ -5,72 +5,75 @@ const cant = document.querySelector('.cant')
 const tipPerson = document.querySelector('.tip-person')
 const totalTips = document.querySelector('.total-person')
 const botones = document.querySelectorAll('.tip-boton')
+const reset = document.querySelector('.reset-boton')
 
-let inprop = 0
-let intotal = 0
 
+let billInput = 0
+let peopleInput = 0
 let porCientos = 0
 
-cant.textContent = ''
+const comprobacion =()=>{
+    if(billInput && peopleInput && porCientos ){
+        reset.classList.add("reset-active")
+    }else{
+        reset.classList.remove("reset-active")
+    }
+}
 
+cant.textContent = ''
 const propina = (total=0,personas=0,porciento=0)=>{
-    const person = Math.floor(total / personas)
-    const prop = person * porciento
+    console.log(total,personas,porciento)
+    const person = Math.floor(Number(total) / Number(personas))
+    const prop = person * Number(porciento)
     const totalF = prop + person
+    
     return [prop,totalF]
 }
-const renderizarA = (e)=>{
+const renderizar = ()=>{
 
-    const propinaIn = propina(e.target.value, people.value, porCientos)[0].toFixed(2)
-    const propinaT = propina(e.target.value, people.value, porCientos)[1].toFixed(2)
-    tipPerson.textContent =  `$ ${propinaIn}`
-    totalTips.textContent =  `$ ${propinaT}`
-
-}
-
-
-const renderizarB = (e)=>{
-    const propinaIn = propina(bill.value ,e.target.value,porCientos)[0].toFixed(2)
-    const propinaT = propina(bill.value ,e.target.value,porCientos)[1].toFixed(2)
-    tipPerson.textContent =  `$ ${propinaIn}`
-    totalTips.textContent =  `$ ${propinaT}`
+    const propinaIn = propina(billInput, +peopleInput, porCientos)[0].toFixed(2)
+    const propinaT = propina(billInput, peopleInput, porCientos)[1].toFixed(2)
+    tipPerson.textContent =  propinaIn ? `$ ${propinaIn}` : 0.00
+    totalTips.textContent =  propinaT ? `$ ${propinaT}` : 0.00
 
 }
-
-
 bill.addEventListener('input',(e)=>{
-    if(!people.value || people.value ==''){
-        people.classList.add("people-active")
-        cant.textContent = "Can't be 0"
-        people.focus()
-        
-    }else if(!bill.value){
-        bill.value = 0
-        renderizarA(e)
 
-    }else{
-        people.classList.remove("people-active")
-        cant.textContent = ''
-        renderizarA(e)
-    }  
+    people.classList.remove("people-active")
+    cant.textContent = ''
+    billInput = Number(e.target.value)
+    renderizar()
+    comprobacion()
+
+    // if(!people.value || people.value ==''){
+    //     people.classList.add("people-active")
+    //     cant.textContent = "Can't be 0"
+    //     renderizar()
+        
+    //     //people.focus()
+        
+    // }else if(!bill.value){
+    //     bill.value = ''
+    //     renderizar()
+    //     comprobacion()
+
+    // }else{
+
+    // }  
 })
 
 people.addEventListener('input',(e)=>{
-    if(!bill.value){
-        renderizarB(e)
-        bill.focus()
-    }else if(!people.value){
-        people.value = 0
-        renderizarB(e)
-
-    }else if(people.classList.contains("people-active")){
-        people.classList.remove("people-active")
-        cant.textContent = ''
-        renderizarB(e)
-
+    if(!bill.value || bill.value ==''){
+        console.log('inserte bill')
     }else{
 
-        renderizarB(e)
+        
+        
+        peopleInput = Number(e.target.value)
+        people.classList.remove("people-active")
+        cant.textContent = ''
+        renderizar()
+        comprobacion()
     }
 })
 
@@ -78,20 +81,51 @@ botones.forEach((boton,i,botons)=>{
     boton.addEventListener('click',(e)=>{
         botones.forEach(bot=>bot.classList.remove("active-boton"))
         boton.classList.add("active-boton")
-        const a = +(boton.textContent.replace('%','')) * 0.01
-        console.log(e.target)
-        porCientos = a 
-        
-
-        
-        
-        
+        const a = Number(boton.textContent.replace('%','')) * 0.01
+        porCientos = a
+        custom.value = '' 
+        custom.blur()
+        renderizar()
+        comprobacion()   
     })
     
 })
+custom.addEventListener('input',(e)=>{
 
-document.addEventListener('keydown',(e)=>{
-    if(e.key === "Enter"){
-        renderizar(e)
+
+    if(!people.value|| !bill.value){
+        console.log('inserte valores')
+    }else{
+
+        
+        botones.forEach(bot=>bot.classList.remove("active-boton"))
+        porCientos = (e.target.value)*0.01
+        renderizar()
+        comprobacion()
     }
+
+})
+
+
+
+
+
+
+reset.addEventListener('click',()=>{
+    if(reset.classList.contains("reset-active")){
+        botones.forEach(bot=>bot.classList.remove("active-boton"))
+        bill.value = ''
+        people.value = ''
+        custom.value =''
+        billInput = ''
+        peopleInput = ''
+        porCientos = ''
+        tipPerson.textContent = 0.00
+        totalTips.textContent = 0.00
+        reset.classList.remove("reset-active")
+        bill.focus()
+
+
+    }
+    
 })
